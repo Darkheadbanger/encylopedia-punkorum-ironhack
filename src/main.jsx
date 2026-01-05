31f8cebdf8b9088436a65b9c8317308fb00fe299
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-06 00:28:44 +0700

    Add Google Fonts import and update font-family in Global.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/Global.css'
 import App from './App.jsx'
+// Import Google Fonts in CSS instead, e.g. in src/styles/Global.css add:
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
