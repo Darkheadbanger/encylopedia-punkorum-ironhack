1124b1ecd7c53c67d98d1c3c7fba152c0b712eee
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-07 21:23:53 +0800

    Enhance Connexion component; add login button and links for registration and password recovery with updated styles

--- src/pages/Auth/Connexion.jsx
+++ src/pages/Auth/Connexion.jsx
@@ -9,6 +9,11 @@ function Connexion() {
         <input type="text" name="username" id="username" />
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
+        <button type="submit">Login</button>
+        <div className="connexion-links">
+          <a href="#">Register</a>
+          <a href="#">Forgot login?</a>
+        </div>
       </form>
     </aside>
   );
