e55db974c193b998a8b23637e0da12a91ebad299
David Bouhaben <davidbouhaben@yahoo.co.id>
2026-01-07 19:33:10 +0800

    Refactor BandsList component; update layout and styling for band information display

--- src/components/BandsList.jsx
+++ src/components/BandsList.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import "../styles/MainPage.css";
+import "../styles/BandsList.css";
 
 function BandsList({band}) {
+  const genreMusic = band.tags.map((genre) => genre.name).filter((genre) => genre.match("punk") || genre.match("hardcore") || genre.match("grindcore") || genre.match("emo") || genre.match("powerviolene")).join(" ")
+  const status = band["life-span"].ended === null ? "Still Active" : `Split-up`
+  const isActiveClass = band["life-span"].ended === null ? "still-active" : "not-active";
+  /*.still-active{
+    color: green;
+}
+
+.not-active{
+    color: red;
+}*/
   return (
-    <div className="band-item">
-      <h3>{band.name}</h3>
-      <p>Country: {band.country || 'Unknown'}</p>
-      <p>Type: {band.type}</p>
+    <div className="band-list-container">
+      <h3 className="band-name">{band.name}</h3>
+      <p>{band.country}</p>
+      <p>{genreMusic}</p>
+      <p className={isActiveClass}> {status}</p>
     </div>
   );
 }
